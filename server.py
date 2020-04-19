5003509192269a8077a5bc2f5fcac066ccd64494
unknown <sta0003@auburn.edu>
2020-04-19 19:52:54 -0400

    tabula rasa

---
+++ server.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('my event', namespace='/socket_space')
+def test_message(message):
+    emit('my response', {'data': message['data']})
+
+@socketio.on('my broadcast event', namespace='/socket_space')
+def test_message(message):
+    emit('my response', {'data': message['data']}, broadcast=True)
+
+@socketio.on('connect', namespace='/socket_space')
+def test_connect(message):
+    print(repr(message))
+    emit('my response', {'data': 'Connected'})
+
+@socketio.on('disconnect', namespace='/socket_space')
+def test_disconnect():
+    print('Client disconnected')
+
+if __name__ == '__main__':
+    socketio.run(app)
