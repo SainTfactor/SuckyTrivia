c7f0d878ec34c9309061912b3dc18051b583c20e
unknown <sta0003@auburn.edu>
2020-04-20 14:16:20 -0400

    Figured out how to pass in get params

--- server.py
+++ server.py
@@ -7,11 +7,12 @@ socketio = SocketIO(app)
 
 @app.route('/')
 def index():
+    session["room_id"] = request.args.get("name")
     return render_template('index.html')
 
 @socketio.on('button_clicked', namespace='/socket_space')
 def test_message(message):
-    print(message)
+    print(session["room_id"])
     emit('my response', {'respy': message['crazy']})
 
 @socketio.on('my broadcast event', namespace='/socket_space')
@@ -20,7 +21,7 @@ def test_message(message):
 
 @socketio.on('connect', namespace='/socket_space')
 def test_connect():
-    print(request.args.get("name"))
+    print()
     emit('my response', {'data': 'Connected'})
 
 @socketio.on('disconnect', namespace='/socket_space')
