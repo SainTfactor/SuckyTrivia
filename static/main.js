c36ead142ef702c688ecd4a812f948d8d8d91c1e
unknown <sta0003@auburn.edu>
2020-04-22 22:06:32 -0400

    I'm dumb

--- static/main.js
+++ static/main.js
@@ -45,8 +45,8 @@ join_game_owner = function(socket, room_code) {
     show_screen("gm_screen");
      
     socket.on("answer_locked", function(data, cb) {
-        $("[data-guid=" + data.guid + "]").css("display", "block");
-        $("[data-guid=" + data.guid + "]").on("click", function(){
+        $("[guid=" + data.guid + "]").css("display", "block");
+        $("[guid=" + data.guid + "]").on("click", function(){
             socket.emit("unlock", { guid: data.guid })
         })
     });   
